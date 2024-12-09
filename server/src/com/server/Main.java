fb192639c76126b66ffc7976add7a4361044bdb7
NullBrunk <anasi10@proton.me>
2024-12-09 13:50:43 +0100

    Start implementing server

--- server/src/com/server/Main.java
+++ server/src/com/server/Main.java
@@ -14,7 +14,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {	
-        System.out.println("Hello from server");
+        Server server = new Server();
+        server.run();
+        server.close();
     }
 
 }
--- server/src/com/server/Server.java
+++ server/src/com/server/Server.java
@@ -1,19 +1,46 @@
 package com.server;
 
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 public class Server {
-    private DatagramSocket serverSocket;
+    private DatagramSocket socket;
     
-    public Server() {
-        
+    public Server() {}
+    public void run() {
         try {
-            this.serverSocket = new DatagramSocket(1337);
-        } catch(SocketException e) {
-            System.out.println("Error while binding port 1337");
-        }
-        System.out.println(this.serverSocket);
+            this.socket = new DatagramSocket(1337);
+            System.out.println("[*] Server listening on udp://0.0.0.0:1337");
 
+            // Récéption des paquets du client
+            byte[] recived_bytes = new byte[256];
+            DatagramPacket receivedPacket = new DatagramPacket(receivedBytes, receivedBytes.length);
+            this.socket.receive(receivedPacket);
+
+            // Récupération du message envoyé par le client
+            String message = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+            System.out.println(message);
+
+            // Envoi de la réponse
+            String response = "Message bien reçu !";
+            byte[] sentBytes = response.getBytes();
+            
+            InetAddress client_addr = receivedPacket.getAddress()
+
+
+
+        } catch(SocketException e) {
+            System.out.println("[ERROR] " + e);
+        } catch(Exception e) {
+            System.out.println("[ERROR] " + e);
+        }
+
+        System.out.println(this.serverSocket);
+    }
+
+    public void close() {
+        if(!serverSocket.isClosed()) serverSocket.close();
     }
 }
