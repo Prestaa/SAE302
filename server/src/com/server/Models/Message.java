0bde288170c03a05d72781396c361d1110c119a0
NullBrunk <anasi10@proton.me>
2024-12-10 13:54:48 +0100

    Start implementing message DAO && SendMessage/GetMessage functionnalities

--- server/src/com/server/Models/Message.java
+++ server/src/com/server/Models/Message.java
@@ -1,7 +1,16 @@
 package com.server.Models;
 
 public class Message {
+    public boolean read = false;
     public User sender;
     public User receiver;
-    public boolean read;
+    public String title;
+    public String body;
+
+    public Message(User sender, User receiver, String title, String body) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.title = title;
+        this.body = body;
+    }
 }
